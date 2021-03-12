e815d49bbdf6f7d50ad3ad796016c118c382a908
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-03-11 20:25:29 -0800

    Update the sample home page into main app

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 function Home() {
-  return <div>Sample Home Page</div>;
+  return (
+    <div>
+      <Typography variant="h3">Productivity Tracker</Typography>
+    </div>
+  );
 }
 
 export default Home;
