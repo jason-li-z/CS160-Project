535a69a2a93a9c50c351306253008884129acaca
Adrian Chan <acadrian40@gmail.com>
2021-03-16 18:52:58 -0700

    Routers added to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+    <Switch>
+        <Route path="/" component={Home} exact/>
+    </Switch>
+    </Router>
   );
 }
 
