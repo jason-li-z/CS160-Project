92cd9d0a5540a63c9a01b08adc592af1916dd428
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-03-17 00:44:53 -0700

    Connected frontend login with backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const databaseURI =
@@ -17,11 +18,18 @@ mongoose
   });
 
 app.use(express.json()); // Use JSON as the Parser
+app.use(cors());
 
 app.get('/', (request, response) => {
   // Test Middleware by reading request body
   console.log(request.body);
   response.json({ sampleText: 'Hello from Default Route' }); // Send "Default Route" response
+});
+
+app.post('/login', (request, response) => {
+  const { username, password } = request.body;
+  console.log(`Username:${username}, password: ${password}`);
+  response.json();
 });
 
 const port = process.env.PORT || 5000; // We are reserving port 3000 for the frontend.
