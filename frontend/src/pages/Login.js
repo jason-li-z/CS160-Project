13e3049718e03c7f9d2317d8898a9a7e0daf08c4
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-03-17 00:44:21 -0700

    Implemented Login Page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import LockIcon from '@material-ui/icons/Lock';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import './Login.css';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState('');
+
+  const handleUserOnChange = (event) => {
+    let user = event.target.value;
+    let regex = new RegExp('^[a-zA-Z0-9_]*$'); // Nonalphanumeric regex
+    let alphanumericValue = regex.exec(user);
+    if (alphanumericValue === null) {
+      // Handle error
+      setError(true);
+      setErrorText('Please use alphanumeric values only');
+      setUsername('');
+    } else {
+      setError(false);
+      setErrorText('');
+      setUsername(alphanumericValue.input);
+    }
+  };
+
+  const handlePasswordOnChange = (event) => {
+    let pw = event.target.value;
+    setPassword(pw);
+  };
+
+  const handleOnClick = async () => {
+    if (username === '' || password === '') {
+      // Validation
+      setError(true);
+      setErrorText('Please check your login information');
+    } else {
+      // Send POST request to backend
+      setError(false);
+      setErrorText('');
+      fetch('http://localhost:5000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username, password: password }),
+      }).then((response) => {
+        if (response.status === 200) {
+          console.log('Success!');
+        }
+      });
+    }
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div className="loginClass">
+      <Paper
+        elevation={12}
+        style={{
+          padding: 40,
+          width: 380,
+          height: 'auto',
+          margin: '20px auto',
+        }}
+      >
+        <Container className={classes.container}>
+          <Typography variant="h5" style={{ paddingRight: 20 }}>
+            User Login
+          </Typography>
+          <Avatar style={{ backgroundColor: '#2A3035' }}>
+            <LockIcon></LockIcon>
+          </Avatar>
+        </Container>
+        <Container className={classes.container} style={{ padding: 20 }}>
+          <TextField
+            error={error}
+            helperText={errorText}
+            label="Username"
+            onChange={handleUserOnChange}
+          ></TextField>
+        </Container>
+        <Container className={classes.container}>
+          <TextField
+            label="Password"
+            type="password"
+            onChange={handlePasswordOnChange}
+          ></TextField>
+        </Container>
+        <Container
+          className={classes.container}
+          style={{ marginTop: 25, padding: 30 }}
+        >
+          <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+            {' '}
+            <Button color="primary" style={{ marginRight: 75 }}>
+              Back Home
+            </Button>
+          </Link>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={handleOnClick}
+          >
+            Login
+          </Button>
+        </Container>
+      </Paper>
+    </div>
+  );
+}
+
+export default Login;
