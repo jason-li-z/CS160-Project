526d4701f0d73bcc17668615a95fb0df74e46442
Adrian Chan <adrian.chan@sjsu.edu>
2021-04-12 20:31:36 -0700

    profile page and user main with css included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Questions from './pages/Questions';
+import UserMain from './pages/UserMain';
+import About from './components/About';
+import Profile from './pages/Profile';
 
 const theme = createMuiTheme({
   typography: {
@@ -22,8 +26,12 @@ function App() {
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/#inline" component={Home}/>
+          <Route path="/#about" component={About}/>
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          {/*<Route path="/questions" component={Questions} />*/}
+          <Route path="/UserMain" component={UserMain} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </Router>
     </ThemeProvider>
