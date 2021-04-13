b9dcd712c36526ecfec94686d426ff2e31505c84
Adrian Chan <adrian.chan@sjsu.edu>
2021-04-12 20:48:52 -0700

    reupdated the corruput App.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Poppins'].join(','),
+  },
+
+  palette: {
+    type: 'light',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/#inline" component={Home}/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
