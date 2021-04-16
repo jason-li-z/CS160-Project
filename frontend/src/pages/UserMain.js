9fd43c0da881d1ebc2cf09e98faa3ad5e0815921
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-04-15 19:51:25 -0700

    Include Logout in UserMain

--- frontend/src/pages/UserMain.js
+++ frontend/src/pages/UserMain.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import NavbarUser from '../components/NavbarUser';
 
-
 function UserMain() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
 
-
-    return (
-        <NavbarUser/>
-    );
+  return <NavbarUser handleLogout={handleLogout} />;
 }
 
 export default UserMain;
