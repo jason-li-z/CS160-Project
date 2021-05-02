f0ef49e0c0a389f06271ca5c9619def5a0d49951
Zichong (Jason) Li <zichongli1998@gmail.com>
2021-05-01 19:59:27 -0700

    Include 24 hours for answers

--- frontend/src/pages/UserMain.js
+++ frontend/src/pages/UserMain.js
@@ -6,6 +6,8 @@ import { Redirect } from 'react-router-dom';
 
 function UserMain() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [valid, setIsValid] = useState(false);
+
   useEffect(() => {
     // Check if token has expired
     const validate = async () => {
@@ -21,7 +23,34 @@ function UserMain() {
         setIsLoggedIn(false);
       }
     };
+
+    const getUserInfo = async () => {
+      let result = await fetch('http://localhost:5000/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: localStorage.getItem('token') }),
+      });
+      let data = await result.json();
+      if (data.status === 200) {
+        let current = new Date(data.data.questionArray[data.data.questionArray.length - 1].date);
+        let next = new Date(current);
+        next.setHours(24, 0, 0, 0);
+        if (next - current <= 0) {
+          setIsValid(true);
+        } else {
+          setIsValid(false);
+        }
+      } else if (data.status === 404) {
+        setIsValid(true);
+      }
+    };
     validate();
+    getUserInfo();
+    // let date = new Date(last.date);
+    // let nextMidnight = date.setHours(24, 0, 0, 0);
+    // console.log(nextMidnight);
   }, []);
 
   if (!isLoggedIn) {
@@ -37,7 +66,15 @@ function UserMain() {
     <Fade in={true} timeout={1000}>
       <div>
         <NavbarUser handleLogout={handleLogout}></NavbarUser>
-        <Userquestion />
+        {valid ? (
+          <Userquestion />
+        ) : (
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '85vh' }}>
+            <Fade in={true}>
+              <h2>You've already answered recently, please come back tomorrow or at 12AM.</h2>
+            </Fade>
+          </div>
+        )}
       </div>
     </Fade>
   );
